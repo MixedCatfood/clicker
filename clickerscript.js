38b9e9d83414bdfa44bc90bfcd1947638c974f21
MixedCatfood <116766712+MixedCatfood@users.noreply.github.com>
2022-12-13 13:59:19 -0500

    Implemented half-way working save function

--- clickerscript.js
+++ clickerscript.js
@@ -1,45 +1,55 @@
 let numberClicks = 0;
+//unit1 is jumbo shrimp
 let unit1 = {
-    name: 'unit1',
+    name: 'Jumbo Shrimp',
     cost: 10,
     production: .1,
     quantity: 0
 };
+//unit2 is crab
 let unit2 = {
-    name: 'unit2',
+    name: 'Crab',
     cost: 100,
     production: 1,
     quantity: 0
-}
+};
+//unit3 is lobster
 let unit3 = {
-    name: 'unit3',
+    name: 'Lobster',
     cost: 1000,
     production: 10,
     quantity: 0
-}
+};
+//unit4 is isopod
 let unit4 = {
-    name: 'unit4',
+    name: 'Isopod',
     cost: 10000,
-    production: 100,
+    production: 50,
     quantity: 0
-}
-let shrimpProduction = unit1.production * unit1.quantity + unit2.production * unit2.quantity + unit3.production * unit3.quantity + unit4.production * unit4.quantity;
+};
+
+let shrimpProduction = 
+(unit1.production * unit1.quantity) + 
+(unit2.production * unit2.quantity) + 
+(unit3.production * unit3.quantity) + 
+(unit4.production * unit4.quantity);
+
 // checks for price and makes sure price is less than score
 function purchase(unit) {
     let price = unit.cost;
     if(price <= numberClicks){
         numberClicks -= price;
         unit.quantity = unit.quantity + 1;
-        unit.cost = Math.floor(unit.cost * 1.5);
+        unit.cost = Math.floor(unit.cost * 1.25);
         shrimpProduction += unit.production;
         document.getElementById('score').innerHTML = `Shrimps: ${Math.floor(numberClicks)}`;
         updateShrimpProductionInHTML();
         updateUnitCostInHTML(unit);
-        if(unit.quantity === 1){
-            setInterval(crustaceanWorking(unit), 1000)
-        }
+        setInterval(crustaceanWorking(unit), 1000);
+        
     }
-}
+};
+
 function updateUnitCostInHTML(unit) {
     switch(unit) {
         case unit1:
@@ -58,18 +68,20 @@ function updateUnitCostInHTML(unit) {
             document.getElementById('unit4Cost').innerHTML = `Price: ${unit4.cost} Shrimps`;
             document.getElementById('unit4QuantityNumber').innerHTML = `${unit4.quantity}`;
     }
-}
+};
+
 function updateShrimpProductionInHTML() {
     document.getElementById('production').innerHTML = `Shrimps per second: ${shrimpProduction.toFixed(1)}`;
-}
+};
+
 // clicking the button makes number go up.... shrimple as that
 function clickButton(){
     numberClicks ++;
     document.getElementById('score').innerHTML = `Shrimps: ${Math.floor(numberClicks)}`;
-}
+};
+
 function idleShrimpProduction() {
     let counter = 0;
-    let target = numberClicks + shrimpProduction;
     let countSpeed = shrimpProduction / 10
     if(shrimpProduction >= 1){
         let i = setInterval(function(){
@@ -84,7 +96,8 @@ function idleShrimpProduction() {
         numberClicks += shrimpProduction;
         document.getElementById('score').innerHTML = `Shrimps: ${Math.floor(numberClicks)}`;
     }
-}
+};
+
 function shrimpClickFunction(){
     clickButton();
     shrimpAnimateForward();
@@ -113,18 +126,21 @@ function crustaceanWorking(unit) {
             if (barWidth >= 100){
                 barWidth = 0;
             } else {
-                barWidth ++;
                 switch(unit) {
                     case unit1:
+                        barWidth += .1;
                         document.getElementById('unit1ProgressBarProgressing').style.width = barWidth + '%';
                         break;
                     case unit2:
+                        barWidth ++;
                         document.getElementById('unit2ProgressBarProgressing').style.width = barWidth + '%';
                         break;
                     case unit3:
+                        barWidth ++;
                         document.getElementById('unit3ProgressBarProgressing').style.width = barWidth + '%';
                         break;
                     case unit4:
+                        barWidth ++;
                         document.getElementById('unit4ProgressBarProgressing').style.width = barWidth + '%';
                 }
             }
@@ -132,11 +148,56 @@ function crustaceanWorking(unit) {
         setInterval(progress, 10);
     }
 }
-// function bakeCookies(){
-//     document.cookie = "numberClicks=" + numberClicks + ";unit1Cost=" + unit1.cost +";unit1Quantity=" + unit1.quantity + ";unit2Cost=" + unit2.cost +";unit2Quantity=" + unit2.quantity + ";unit3Cost=" + unit3.cost + ";unit3Quantity=" + unit3.quantity + ";unit4Cost=" + unit4.cost + ";unit4Quantity=" + unit4.quantity + ";expires";
-//     alert("Cookies Saved");
-// }
-// function loadCookies(){}
-// setInterval(bakeCookies, 1000 * 60);
-// setInterval(updateScore, 1000);
+
+function saveGame(){
+    localStorage.setItem("score", `${numberClicks}`);
+
+    localStorage.setItem("unit1Cost", `${unit1.cost}`);
+    localStorage.setItem("unit1Production", unit1.production);
+    localStorage.setItem("unit1Quantity", unit1.quantity);
+
+    localStorage.setItem("unit2Cost", `${unit2.cost}`);
+    localStorage.setItem("unit2Production", unit2.production);
+    localStorage.setItem("unit2Quantity", unit2.quantity);
+
+    localStorage.setItem("unit3Cost", unit3.cost);
+    localStorage.setItem("unit3Production", unit3.production);
+    localStorage.setItem("unit3Quantity", unit3.quantity);
+
+    localStorage.setItem("unit4Cost", unit4.cost);
+    localStorage.setItem("unit4Production", unit4.production);
+    localStorage.setItem("unit4Quantity", unit4.quantity);
+};
+
+function loadGame(){
+    numberClicks = parseInt(localStorage.getItem("score"));
+
+    let unit1Cost = parseInt(localStorage.getItem("unit1Cost"));
+    unit1.cost = unit1Cost;
+    let unit1Production = parseInt(localStorage.getItem("unit1Production"));
+    unit1.production = unit1Production;
+    let unit1Quantity = parseInt(localStorage.getItem("unit1Quantity"));
+    unit1.quantity = unit1Quantity;
+
+    unit2.cost = parseInt(localStorage.getItem("unit2Cost"));
+    unit2.production = parseInt(localStorage.getItem("unit2Production"));
+    unit2.quantity = parseInt(localStorage.getItem("unit2Quantity"));
+
+    unit3.cost = parseInt(localStorage.getItem("unit3Cost"));
+    unit3.production = parseInt(localStorage.getItem("unit3Production"));
+    unit3.quantity = parseInt(localStorage.getItem("unit3Quantity"));
+
+    unit4.cost = parseInt(localStorage.getItem("unit4Cost"));
+    unit4.production = parseInt(localStorage.getItem("unit4Production"));
+    unit4.quantity = parseInt(localStorage.getItem("unit4Quantity"));
+
+    updateUnitCostInHTML(unit1);
+    updateUnitCostInHTML(unit2);
+    updateUnitCostInHTML(unit3);
+    updateUnitCostInHTML(unit4);
+    document.getElementById('score').innerHTML = `Shrimps: ${Math.floor(numberClicks)}`;
+    updateShrimpProductionInHTML();
+};
+// loadGame();
+setInterval(saveGame, 1000 * 30);
 setInterval(idleShrimpProduction, 1000);
